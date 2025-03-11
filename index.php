af045de9d444920598d78078756ecd1f13b24e59
bedi <bedstephane.tiekotieko@student.hepl.be>
2025-03-12 00:12:10 +0100

    test p

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
                 <li><a href="about.php">À propos</a></li>
                 <li><a href="services.php">Services</a></li>
                 <li><a href="contact.php">Contact</a></li>
+                <p>test</p>
             </ul>
         </nav>
     </header>
