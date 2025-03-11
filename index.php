5bfed2dfb556ce227e00bb425caeb43540cfc0af
cemilserttas <cemil.serttas@student.hepl.be>
2025-03-12 00:00:38 +0100

    Le premier structure du projet avec des répertoires et fichiers.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Greenway - ride with us</title>
+</head>
+<body>
+    <header>
+        <!-- TODO: Barre de navigation avec logo clickable (accueil) et menu -->
+        <nav>
+            <a href="index.php">
+                <img src="assets/img/GreenWay_logo" alt="Greenway Logo" style="height: 50px;">
+            </a>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="about.php">À propos</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main><!-- TODO: Contenu de la page --></main>
+    <footer><!-- TODO: Pied de page avec le logo clickable (accueil) et  --></footer>
+</body>
+</html>
