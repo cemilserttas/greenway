b526592027c47a520fcf8e1effc5f2714ef2b004
cemilserttas <cemil.serttas@student.hepl.be>
2025-04-22 23:02:38 +0200

    Todo a propos et cantact

--- src/components/faq.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.faq-question').forEach(question => {
-    question.addEventListener('click', () => {
-      const answer = question.nextElementSibling;
-      const icon = question.querySelector('.icon');
-  
-      answer.classList.toggle('show');
-      icon.classList.toggle('rotate');
-    });
-  });
--- src/components/header.js
+++ src/components/header.js
@@ -10,8 +10,10 @@ const HeaderComponent = {
           <ul>
             <li v-if="isConnected"><a href="/pages/formulaire_trajet.html">Proposer un trajet</a></li>
             <li><a href="/pages/contact.html">Contact</a></li>
+            <li><a href="/pages/a-propos.html">A Propos</a></li>
             <li v-if="!isConnected"><a href="/pages/connexion.html" class="btn-login">Connexion</a></li>
             <li v-if="isConnected"><a href="/pages/logout.php" class="btn-logout">Déconnexion</a></li>
+            <li v-if="isConnected"><a href="/pages/parametres_profil.html" class="btn-logout">Profil</a></li>
           </ul>
         </nav>
       </div>
